d1e2d20e5c913cd8bd42d39b986e09765904a55f
Иван Шаменков <nickeny@yandex.ru>
2024-11-01 14:45:02 +0300

    add API

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,13 @@
 import 'vue3-emoji-picker/css';
 import '@/assets/styles/styles.scss';
 
+import { useRouter } from "vue-router";
+import { globalRouter } from './globalRouter';
+
 import ModalMain from './components/modals/ModalMain.vue';
+
+const router = useRouter();
+globalRouter.router = router;
 
 </script>
 
---
+++ client/src/api/api.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { globalRouter } from "../globalRouter";
+
+const HOST = String( process.env.API_URL ) + process.env.API_PREFIX;
+
+
+const $axios = axios.create({
+	baseURL: HOST,
+	withCredentials: true,
+});
+
+$axios.interceptors.response.use(
+	function (response) {
+		return response;
+	},
+	function (error) {
+		console.log(globalRouter);
+		if (!!error.response && error.
+		response.status === 401) {
+
+			globalRouter.router?.push('/login');
+
+		}
+		return Promise.reject(error);
+	}
+);
+
+export default $axios;
